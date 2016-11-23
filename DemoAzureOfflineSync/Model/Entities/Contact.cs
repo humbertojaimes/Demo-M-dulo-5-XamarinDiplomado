68a66d75e73ce983ea460587e7c7f493a1d4c02f
Humberto Jaimes <humberto.jaimes@outlook.com>
2016-11-22 18:32:46 -0600

    Add Files to project

---
+++ DemoAzureOfflineSync/Model/Entities/Contact.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+
+namespace DemoAzureOfflineSync
+{
+    [DataTable("Contacts")]
+    public class Contact
+    {
+        [JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [Version]
+        public string Version { get; set; }
+    }
+}
---
+++ DemoAzureOfflineSync/Model/Services/AzureClient.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DemoAzureOfflineSync
+{
+    public class AzureClient
+    {
+        private IMobileServiceClient _client;
+        private IMobileServiceTable<Contact> _table;
+		private const string serviceUri= "";
+        public AzureClient()
+        {
+			_client = new MobileServiceClient(serviceUri);
+
+            _table = _client.GetTable<Contact>();
+        }
+
+        public async Task<IEnumerable<Contact>> GetContacts()
+        {
+            var empty = new Contact[0];
+            try
+            {
+                return await _table.ToEnumerableAsync();
+            }
+            catch (Exception ex)
+            {
+                return empty;
+            }
+        }
+
+		public async void AddContact(Contact contact) 
+		{
+			await _table.InsertAsync(contact);
+
+		}
+
+    }
+}
